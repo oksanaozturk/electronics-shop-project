e4638a4288d8d7eded55a0ea8ba70305c974d070
Oksana Ozturk <oksanaozturk@gmail.com>
2023-11-12 23:02:28 +0300

    update item.py and written tests for task No. 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+
+# TestCase#1 конструктора-инициализатора Класса Item
+assert item1.name == "Смартфон"
+assert item1.price == 10000
+assert item1.quantity == 20
+
+# TestCase#2 для метода calculate_total_price
+assert item1.calculate_total_price() == 200000
+
+# TestCase#3 для метода apply_discount
+Item.pay_rate = 0.8
+item1.apply_discount()
+assert item1.price == 8000.0
