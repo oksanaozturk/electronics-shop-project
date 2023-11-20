e45cb576a170ec4a14930591f225c5c2d42443bb
Oksana Ozturk <oksanaozturk@gmail.com>
2023-11-20 23:20:33 +0300

    update test_item.py add test_repr and test_str

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,50 +1,50 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
-
-item1 = Item("Смартфон", 10000, 20)
+import os
 
 
 @pytest.fixture
 def fixture_item():
     """Фикстура для тестовых функций"""
-    return item1
+
+    return Item("Смартфон", 10000, 20)
 
 
 def test__init__(fixture_item):
     """Тест конструктора-инициализатора Класса Item"""
 
-    assert item1.name == "Смартфон"
-    assert item1.price == 10000
-    assert item1.quantity == 20
+    assert fixture_item.name == "Смартфон"
+    assert fixture_item.price == 10000
+    assert fixture_item.quantity == 20
 
 
 def test_calculate_total_price(fixture_item):
     """Тест для метода calculate_total_price"""
 
-    assert item1.calculate_total_price() == 200000
+    assert fixture_item.calculate_total_price() == 200000
 
 
 def test_apply_discount(fixture_item):
     """Тест для метода apply_discount"""
 
     Item.pay_rate = 0.8
-    item1.apply_discount()
-    assert item1.price == 8000.0
+    fixture_item.apply_discount()
+    assert fixture_item.price == 8000.0
 
 
 def test_name(fixture_item):
     """
     Тест для Сеттер @property,который делает проверку имени на соответствие заданному параметру длины
     """
-    assert item1.name == 'Смартфон'
-    item1.name = 'Электроприбор'
-    assert item1.name == 'Электропри'
+    assert fixture_item.name == 'Смартфон'
+    fixture_item.name = 'Электроприбор'
+    assert fixture_item.name == 'Электропри'
 
 
 def test_instantiate_from_csv():
     """Тест для Kласс-метода, инициализирующий экземпляры класса `Item` данными из файла src/items.csv"""
-    Item.instantiate_from_csv('items.csv')
+    Item.instantiate_from_csv(os.path.join('..', 'src', 'items.csv'))
     assert len(Item.all) == 5
     assert Item.all[1].name == 'Ноутбук'
     assert Item.all[2].price == 10
@@ -57,3 +57,11 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+
+def test_repr(fixture_item):
+    assert repr(fixture_item) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str(fixture_item):
+    assert str(fixture_item) == 'Смартфон'
