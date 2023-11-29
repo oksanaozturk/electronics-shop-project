f1f89610d8cada901164159c35ef7da08e540530
Oksana Ozturk <oksanaozturk@gmail.com>
2023-11-29 16:48:29 +0300

    Update item.py and phone.py

--- src/phone.py
+++ src/phone.py
@@ -7,28 +7,28 @@ class Phone(Item):
        """
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
-        """Инициализация экземпляров класса
+        """Инициализация атрибутов класса
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        :param number_of_sim: Количество сим-карт
+            :param name: Название товара.
+            :param price: Цена за единицу товара.
+            :param quantity: Количество товара в магазине.
+            :param number_of_sim: Количество сим-карт
         """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
     @property
-    def number_of_sim(self):
+    def number_of_sim(self) -> int:
         """Создание метода геттер для number_of_sim"""
         return self.__number_of_sim
 
     @number_of_sim.setter
-    def number_of_sim(self, num_sim):
+    def number_of_sim(self, num_sim: int) -> None:
         """Создание метода сеттер для number_of_sim, для проверки входящего значения количества карт"""
         if not isinstance(num_sim, int) or num_sim <= 0:
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
         self.__number_of_sim = num_sim
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """ Магический метод __repr__, выводит информацию для программистов"""
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
