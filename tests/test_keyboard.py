b6efc3aa829f3f0ad7a5d628131946532e373c89
Oksana Ozturk <oksanaozturk@gmail.com>
2023-12-10 20:25:16 +0300

    update test_keyboard.py add test_language

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test__init__(fixture_keyboard):
     """Тест конструктора-инициализатора Класса Keyboard"""
 
@@ -14,8 +17,15 @@ def test__str__(fixture_keyboard):
 
 
 def test_change_lang(fixture_keyboard):
+    """Тест для проверки метода пор смене языка"""
     fixture_keyboard.change_lang()
     assert str(fixture_keyboard.language) == "RU"
 
     fixture_keyboard.change_lang()
     assert str(fixture_keyboard.language) == "EN"
+
+
+def test_language(fixture_keyboard):
+    """Тест для проверки работы геттера при отсутвии сеттера(нельза сменить язык на другой, приватный аргумент)"""
+    with pytest.raises(AttributeError):
+        fixture_keyboard.language = 'CH'
